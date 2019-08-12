2bb17b6bff4163e07c9aaa21305a0b6e6c36f111
zibiuss <zibius4@gmail.com>
2019-08-12 14:55:02 +0300

    Snek

---
+++ Snek/Snek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snek
+{
+    class Program
+    {
+        static void Draw(int x,int y,char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 3;
+            char sym1 = '*';
+            Draw(x, y, sym1);
+            Console.ReadKey();
+        }
+    }
+}
